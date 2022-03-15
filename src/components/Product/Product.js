2255a8f0de4ed9aed0537be065cdf6866ceee53f
Igor Batko <igor.batko.batko@gmail.com>
2022-03-15 15:40:32 +0100

    Add useMemo

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,6 +3,7 @@ import ProductImage from '../ProductImage/ProductImage';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import ProductOptions from '../ProductOptions/ProductOptions';
+import { useMemo } from 'react';
 
 
 const Product = props => {
@@ -10,12 +11,8 @@ const Product = props => {
   const [currentColor, setCurrentColor] = useState(props.colors[0]);
   const [currentSize, setCurrentSize] = useState(props.sizes[0]);
 
+  const price = useMemo (() => props.basePrice + currentSize.additionalPrice, [currentSize])
  
-
-  const getPrice = () => {
-    return(props.basePrice + currentSize.additionalPrice)
-  }
-
   const summary = e =>{
     e.preventDefault();
 
@@ -23,7 +20,7 @@ const Product = props => {
       console.log('Summary'),
       console.log('================'),
       console.log('Name: ', props.title),
-      console.log('Price: ', getPrice()),
+      console.log('Price: ', price),
       console.log('Size: ', currentSize.name),
       console.log('Color: ', currentColor)
     )
@@ -35,7 +32,7 @@ const Product = props => {
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>Price: {getPrice()}$</span>
+          <span className={styles.price}>Price: {price}$</span>
         </header>
         <ProductOptions 
         currentColor={currentColor}
